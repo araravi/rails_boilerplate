ac9a0c28f196334c6f356d0dbd4797cf842879c5
ara <aravindh.shankar.91@gmail.com>
2017-06-19 16:53:28 +0800

    Update

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,17 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery with: :exception
+  include DeviseTokenAuth::Concerns::SetUserByToken
+  before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :set_locale
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :role, :phone])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:name, :role, :phone])
+  end
+
+  def set_locale
+    I18n.locale = current_user.try(:locale) || I18n.default_locale
+  end
+
 end
